7d227464485202db2bc94b6ec978744390d01f5a
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2026-08-28 21:45:48 +0100

    refactor: improve encapsulation, immutability, and culture-invariant serialization
    - QueryEndpointBase.cs: change constructors from public to protected
    - ApiConnection.cs: add comments, clarify session reuse/reconnect/refresh logic
    - ApiSession.cs: make EnsureLoginResponse, EnsureTokenResponse, ThrowInvalidOperation static
    - Index.razor.cs: make _formattedOptions readonly, ConvertDateToTimeStamp static
    - CustomDateTimeConverter.cs, JsonStringToDecimalConverter.cs: use CultureInfo.InvariantCulture for serialization/parsing
    - CustomDateTimeConverter.cs: fix using directives and formatting

--- src/ApiBureau.Devyce.Api/Converters/CustomDateTimeConverter.cs
+++ src/ApiBureau.Devyce.Api/Converters/CustomDateTimeConverter.cs
@@ -12,7 +12,7 @@ public class CustomDateTimeConverter : JsonConverter<DateTime>
 
     public override void Write(Utf8JsonWriter writer, DateTime date, JsonSerializerOptions options)
     {
-        writer.WriteStringValue(date.ToString(_format));
+        writer.WriteStringValue(date.ToString(_format, CultureInfo.InvariantCulture));
     }
 
     public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
